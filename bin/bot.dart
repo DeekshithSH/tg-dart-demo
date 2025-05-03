0e4e50ab500fe50e6cd6397a50cdd235c35d7523
Deekshith SH <shdeekshith@gmail.com>
2025-05-03 16:28:54 +0530

    Authorize Bot

--- bin/bot.dart
+++ bin/bot.dart
@@ -1,5 +1,25 @@
-import 'package:bot/bot.dart' as bot;
+import 'conf.dart';
+import 'package:t/t.dart' as t;
+import 'package:bot/client.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${bot.calculate()}!');
+Future<void> main() async {
+  final session = TelegramSessionClient(apiId: apiId, apiHash: apiHash);
+
+  await session.connect();
+  await Future.delayed(Duration(seconds: 1));
+  await session.start(botToken: botAuthToken);
+  print("Account authorized");
+
+  final client = session.client;
+  if (client == null) throw Exception("Client is null");
+
+  final result = await client.users.getFullUser(id: t.InputUserSelf());
+  final user = result.result;
+  if (user is t.UsersUserFull && user.users.isNotEmpty) {
+    final me = user.users[0] as t.User;
+    print("Hello, I'm ${me.firstName}");
+    print("You can find me using: @${me.username}");
+  } else {
+    print("Could not fetch user info");
+  }
 }
---
+++ lib/client.dart
@@ -0,0 +1,121 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:t/t.dart' as t;
+import 'package:tg/tg.dart' as tg;
+
+class TelegramSessionClient {
+  final int apiId;
+  final String apiHash;
+  final void Function(t.UpdatesBase update)? onUpdate;
+
+  Socket? _socket;
+  StreamController<Uint8List>? _sender;
+  StreamController<Uint8List>? _receiver;
+  late tg.Obfuscation _obf;
+  tg.Client? _client;
+
+  TelegramSessionClient({
+    required this.apiId,
+    required this.apiHash,
+    this.onUpdate,
+  });
+
+  Future<void> connect([String ip = '149.154.167.50', int port = 443]) async {
+    await close();
+
+    print("Connecting to $ip:$port");
+    _obf = tg.Obfuscation.random(false, 4);
+    _socket = await Socket.connect(ip, port);
+    print("Done");
+    _sender = StreamController<Uint8List>();
+    _receiver = StreamController<Uint8List>.broadcast();
+
+    _sender!.stream.listen(_socket!.add);
+    _socket!.listen(_receiver!.add);
+
+    _client = tg.Client(
+      sender: _sender!.sink,
+      receiver: _receiver!.stream,
+      obfuscation: _obf,
+      session: null,
+    );
+
+    if (onUpdate != null) {
+      _client!.stream.listen(onUpdate!);
+    }
+
+    await _client!.initConnection<t.Config>(
+      apiId: apiId,
+      deviceModel: Platform.operatingSystem,
+      appVersion: "1.0.0",
+      systemVersion: Platform.operatingSystemVersion,
+      systemLangCode: "en",
+      langCode: "en",
+      langPack: "",
+      proxy: null,
+      params: null,
+      query: t.HelpGetConfig(),
+    );
+  }
+
+  Future<t.Result<t.AuthAuthorizationBase>> start({required String botToken}) async {
+    final c = _client;
+    if (c == null) throw Exception("Client not connected");
+
+    final auth = await c.auth.importBotAuthorization(
+      flags: 0,
+      apiId: apiId,
+      apiHash: apiHash,
+      botAuthToken: botToken,
+    );
+
+    if (auth.error == null) return auth;
+
+    if (auth.error?.errorCode == 303) {
+      final configResult = await c.help.getConfig();
+      final config = configResult.result;
+      if (config is! t.Config) {
+        throw Exception("Unexpected config type: ${config.runtimeType}");
+      }
+
+      final dcOptions = config.dcOptions.cast<t.DcOption>();
+      final match = RegExp(r'\d+').firstMatch(auth.error!.errorMessage);
+      final dcId = int.parse(match!.group(0)!);
+      final dc = dcOptions.firstWhere(
+        (opt) => opt.id == dcId && !opt.ipv6 && !opt.mediaOnly,
+      );
+
+      await connect(dc.ipAddress, dc.port);
+      await Future.delayed(Duration(seconds: 1));
+      return await start(botToken: botToken);
+    } else {
+      throw Exception("Unhandled Error: ${auth.error}");
+    }
+  }
+
+  tg.Client? get client => _client;
+
+  Future<void> close() async {
+    await _sender?.close();
+    await _receiver?.close();
+    await _socket?.close();
+    _client = null;
+    _socket = null;
+    _sender = null;
+    _receiver = null;
+  }
+}
+
+t.DcOption findDcOption({
+  required List<t.DcOption> dcOptions,
+  required int dcId,
+  bool ipv6 = false,
+  bool mediaOnly = false,
+}) {
+  return dcOptions.firstWhere(
+    (item) =>
+        item.id == dcId && (item.ipv6 == ipv6) && item.mediaOnly == mediaOnly,
+  );
+}
