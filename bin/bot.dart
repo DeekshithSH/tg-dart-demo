7f5ca52426dc46793130baed96f0d45b391c8895
Deekshith SH <shdeekshith@gmail.com>
2025-05-03 21:13:34 +0530

    Echo Bot

--- bin/bot.dart
+++ bin/bot.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:t/t.dart' as t;
 import 'package:bot/client.dart';
 
@@ -5,9 +8,9 @@ final apiId = YOUR_API_ID;
 final apiHash = 'YOUR_API_HASH';
 final botToken = 'YOUR_BOT_TOKEN';
 
-Future<void> main() async {
-  final session = TelegramSessionClient(apiId: apiId, apiHash: apiHash);
+final session = TelegramSessionClient(apiId: apiId, apiHash: apiHash, onUpdate: handleUpdates);
 
+Future<void> main() async {
   await session.connect();
   await Future.delayed(Duration(seconds: 1));
   await session.start(botToken: botToken);
@@ -25,4 +28,57 @@ Future<void> main() async {
   } else {
     print("Could not fetch user info");
   }
+}
+
+void handleUpdates(t.UpdatesBase update) async{
+  if(update is t.Updates){
+    for (final message in update.updates){
+      if (message is t.UpdateNewMessage){
+        final msg = message.message;
+        if (msg is t.Message){
+          // For now only private message
+          t.User? user;
+          t.User? chat;
+          if (msg.peerId is t.PeerUser){
+            final peer = msg.peerId as t.PeerUser;
+            chat = update.users.whereType<t.User>().firstWhere((u) => u.id == peer.userId);
+          }
+          if (msg.fromId is t.PeerUser){
+            final peer = msg.peerId as t.PeerUser;
+            user = update.users.whereType<t.User>().firstWhere((u) => u.id == peer.userId);
+          }
+          if (chat != null) {
+            handleMessage(msg, user, chat);
+          }
+        }
+      }
+    }
+  }
+}
+
+Future handleMessage(t.Message message, t.User? user, t.User chat) async{
+  final client = session.client;
+  if (client == null) return ;
+  await client.messages.sendMessage(
+    noWebpage: false,
+    silent: false,
+    background: false,
+    clearDraft: false,
+    noforwards: false,
+    updateStickersetsOrder: false,
+    invertMedia: false,
+    allowPaidFloodskip: false,
+    peer: t.InputPeerUser(userId: chat.id, accessHash: chat.accessHash ?? 0),
+    message: message.message,
+    randomId: generateRandomId()
+  );
+}
+
+int generateRandomId() {
+  final random = Random.secure();
+  final buffer = Uint8List(8);
+  for (int i = 0; i < 8; i++) {
+    buffer[i] = random.nextInt(256);
+  }
+  return ByteData.sublistView(buffer).getInt64(0, Endian.little);
 }
